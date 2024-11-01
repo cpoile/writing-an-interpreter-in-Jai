9e263d2dacff1b04057c0d53c5f81e0985f5de10
Christopher Poile <cpoile@gmail.com>
2024-11-01 19:25:46 -0400

    first part of ch 1.3

---
+++ make.bat
@@ -0,0 +1,5 @@
+@echo off
+
+if not exist c:\Users\Chris\git\jai\writing-an-interpreter\build mkdir c:\Users\Chris\git\jai\writing-an-interpreter\build
+
+jai c:\Users\Chris\git\jai\writing-an-interpreter\main.jai -output_path c:\Users\Chris\git\jai\writing-an-interpreter\build
