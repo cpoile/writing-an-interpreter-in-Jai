077935f74a43bfca6d91c24c4b6075869ed6630c
Christopher Poile <cpoile@gmail.com>
2024-11-10 14:08:47 -0500

    adding some dir-locals; pre big-refactoring/simplification

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((jai-mode . ((custom-compile-cmd . "c:/Users/Chris/git/jai/writing-an-interpreter/make.bat")
+              (custom-run-all-cmd . "c:/Users/Chris/git/jai/writing-an-interpreter/build/main.exe")
+              (custom-run-test-cmd . "c:/Users/Chris/git/jai/writing-an-interpreter/build/main.exe test"))))
