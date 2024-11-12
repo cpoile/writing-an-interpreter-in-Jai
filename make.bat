6d994e566e75b60df775e3d463ce3f67814ea764
Christopher Poile <cpoile@gmail.com>
2024-11-11 22:48:53 -0500

    refactoring file layout; tried ast structs, didn't work, reverted.

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@
 
 if not exist c:\Users\Chris\git\jai\writing-an-interpreter\build mkdir c:\Users\Chris\git\jai\writing-an-interpreter\build
 
-jai c:\Users\Chris\git\jai\writing-an-interpreter\main.jai -output_path c:\Users\Chris\git\jai\writing-an-interpreter\build
+jai c:\Users\Chris\git\jai\writing-an-interpreter\src\main.jai -output_path c:\Users\Chris\git\jai\writing-an-interpreter\build
